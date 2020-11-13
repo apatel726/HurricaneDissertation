50147138156cef28602b2477bf4361c5c8fd15bf
VM <akash0726@gmail.com>
2020-11-13 16:17:55 +0000

    separated ingest and inference files

--- deploy.py
+++ deploy.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Created on Tue Sep 15 01:04:55 2020
 @author: Hammad, Akash, Jonathan
@@ -24,159 +25,12 @@ import hurricane_ai.plotting_utils
 from typing import List, Dict
 
 from hurricane_ai.ml.bd_lstm_td import BidrectionalLstmHurricaneModel
-
-def past_track(link):
-    '''
-    From a KMZ file of a storm in the NHC format, we extract the history
-    Parameters
-    ----------
-    link string
-        The network link or downloadable KMZ href file
-    Returns
-    -------
-    dict
-    '''
-    kmz = requests.get(link)
-    uncompressed = zipfile.ZipFile(io.BytesIO(kmz.content))
-
-    # get the kml name
-    for name in uncompressed.namelist():
-        # all kml file names begin with al, e.g. 'al202020.kml'
-        if name[:2] == 'al':
-            file_name = name
-
-    # read the contents of the kml file in the archive
-    kml = xmltodict.parse(uncompressed.read(file_name))
-    kml['results'] = []
-    for attribute in kml['kml']['Document']['Folder']:
-        if attribute['name'] == 'Data':
-            for entry in attribute['Placemark']:
-                # parse time information
-                time = datetime.strptime(entry['atcfdtg'],
-                                        '%Y%m%d%H').replace(
-                    tzinfo=timezone('UTC'))
-
-                # add to results
-                kml['results'].append({
-                    'time' : time,
-                    'wind' : float(entry['intensity']),
-                    'lat' : float(entry['lat']),
-                    'lon' : float(entry['lon']),
-                    'pressure' : float(entry['minSeaLevelPres'])
-                })
-                print(kml['results'][-1])
-
-    return kml
-
-def nhc() -> List[Dict[str, List]]:
-    '''
-    Runs the NHC update and populates current Atlantic storms
-    Returns
-    -------
-    array of dict
-        Each dictionary is in the following form,
-        {
-            "storm" : string # the storm ID from the NHC
-            "metadata" : dict # the kml files used to create the results
-            "entries" : array of dict # The data for the storm in the form,
-                {
-                    'time' : Datetime,
-                    'wind' : Knots,
-                    'lat' : Decimal Degrees,
-                    'lon' : Decimal Degrees,
-                    'pressure' : Barometric pressure (mb)
-                }
-        }
-    '''
-    # this link can be reused to download the most recent data
-    static_link = 'https://www.nhc.noaa.gov/gis/kml/nhc_active.kml'
-    # common timezones for parsing with dateutil. offset by seconds
-    timezones = {
-        "ADT": 4 * 3600,
-        "AST": 3 * 3600,
-        "CDT": -5 * 3600,
-        "CST": -6 * 3600,
-        "CT": -6 * 3600,
-        "EDT": -4 * 3600,
-        "EST": -5 * 3600,
-        "ET": -5 * 3600,
-        "GMT": 0 * 3600,
-        "PST": -8 * 3600,
-        "PT": -8 * 3600,
-        "UTC": 0 * 3600,
-        "Z": 0 * 3600,
-    }
-
-    # create data structure as dictionary
-    request = requests.get(static_link)
-    data = xmltodict.parse(request.text)
-    results = []
-    
-    # return if no storms
-    if 'Folder' not in data['kml']['Document'].keys() :
-        return
-    
-    # parse in storms
-    for folder in data['kml']['Document']['Folder']:
-        # the id's that start with 'at' are the storms we are interested in
-        # others can include 'wsp' for wind speed probabilities
-        if folder['@id'][:2] == 'at':
-            # some storms don't have any data because they are so weak
-            if not 'ExtendedData' in folder.keys():
-                continue
-
-            # storm data structure
-            storm = {
-                'metadata': folder,
-                'entries': []
-            }
-            entry = {}
-
-            for attribute in folder['ExtendedData'][1]:
-                if attribute == 'tc:atcfID':  # NHC Storm ID
-                    storm['id'] = folder['ExtendedData'][1][attribute]
-                elif attribute == 'tc:name':  # Human readable name
-                    print(folder['ExtendedData'][1][attribute])
-                elif attribute == 'tc:centerLat':  # Latitude
-                    entry['lat'] = float(folder['ExtendedData'][1][attribute])
-                elif attribute == 'tc:centerLon':  # Longitude
-                    entry['lon'] = float(folder['ExtendedData'][1][attribute])
-                elif attribute == 'tc:dateTime':  # Timestamp
-                    entry['time'] = dateutil.parser.parse(
-                        folder['ExtendedData'][1][attribute],
-                        tzinfos=timezones)
-                elif attribute == 'tc:minimumPressure':  # Barometric pressure
-                    entry['pressure'] = float(folder['ExtendedData'][1]
-                                              [attribute].split(' ')[0])
-                elif attribute == 'tc:maxSustainedWind':  # Wind Intensity
-                    # note that we are converting mph to knots
-                    entry['wind'] = float(folder['ExtendedData'][1][attribute].
-                                          split(' ')[0]) / 1.151
-
-            print(storm['id'])
-            print(entry)
-
-            # add entry to storm
-            storm['entries'].append(entry)
-            # get network link and extract past history
-            for links in folder['NetworkLink']:
-                if links['@id'] == 'pasttrack':
-                    kml = past_track(links['Link']['href'])
-                    # add history to entries
-                    storm['entries'].extend(kml['results'])
-
-                    # add history to storm metadata
-                    storm['metadata']['history'] = kml
-
-            # add to results
-            results.append(storm)
-
-    return results
-
+from ingest import *
 
 def prep_hurricane_data(observations: List, lag: int) -> pd.DataFrame:
     """
     Converts raw observations to data frame and computes derived features.
+
     :param observations: Raw hurricane kinematic and barometric measurements.
     :param lag: Number of observation intervals to lag derived features.
     :return: Data frame of raw and derived hurricane measurements.
@@ -224,6 +78,7 @@ def prep_hurricane_data(observations: List, lag: int) -> pd.DataFrame:
 def run_live_inference(base_directory: str, model_file: str, scaler_file: str) -> None:
     """
     Pulls live storm data and runs single pass inference for every storm.
+
     :param base_directory: Path to directory containing serialized artifacts (e.g. models, scalers).
     :param model_file: Filename of the model file.
     :param scaler_file: Filename of the scaler file.
@@ -233,7 +88,7 @@ def run_live_inference(base_directory: str, model_file: str, scaler_file: str) -
     lag = 5
 
     # Initialize model
-    model = BidrectionalLstmHurricaneModel((None, None), "wind", os.path.join(base_directory, scaler_file),
+    model = BidrectionalLstmHurricaneModel((None, None), "lat", os.path.join(base_directory, scaler_file),
                                            model_path=os.path.join(base_directory, model_file))
 
     # Grab live storm data
@@ -246,7 +101,7 @@ def run_live_inference(base_directory: str, model_file: str, scaler_file: str) -
         df = prep_hurricane_data(storm["entries"], lag)
        # df2 = df.iloc[[0, -1]]
         
-        if (len(storm["entries"])) <= 20 : # 1 entry = 6 hours, 20 entries is 120 hours (5 days)
+        if (len(storm["entries"])) < 20 : # 1 entry = 6 hours, 20 entries is 120 hours (5 days)
             print(f'{storm["metadata"]["name"]} does not have enough data (minimum 5 days)')
             continue
         
@@ -263,6 +118,7 @@ def run_live_inference(base_directory: str, model_file: str, scaler_file: str) -
           scaler = pickle.load(f)
         
         # Run inference based on type of model
+        print(root['universal'])
         dictimport = root["config"]  
         if root['universal'] :
             # Print result
@@ -274,8 +130,7 @@ def run_live_inference(base_directory: str, model_file: str, scaler_file: str) -
                 
                 # wind prints the wind for the first 3 days with an input shape of 11 features
                 wind_result = []
-                print(result.shape)
-                wind_result.append(hurricane_ai.plotting_utils._generate_sparse_feature_vector(11, 2, result[day][wind_index])) 
+                wind_result.append(hurricane_ai.plotting_utils._generate_sparse_feature_vector(11, 2, result[day][wind_index])) #
                 print(f'{day + 1} day: universal result wind test:{scaler.inverse_transform(wind_result)[0][2]}')
                 
                 # lat prints the wind for the first 3 days with an input shape of 11 features
@@ -287,7 +142,7 @@ def run_live_inference(base_directory: str, model_file: str, scaler_file: str) -
                 long_result = []
                 long_result.append(hurricane_ai.plotting_utils._generate_sparse_feature_vector(11, 1, result[day][long_index]))
                 print(f'{day + 1} day: universal result long test:{scaler.inverse_transform(long_result)[0][1]}')
-            
+            f.close() #closes scaler open
         elif not root['universal'] :
             featuresearch = dictimport["name"]
             
@@ -295,7 +150,7 @@ def run_live_inference(base_directory: str, model_file: str, scaler_file: str) -
                 if featuresearch == "sequential":
                     model = "wind"
                     wind_result = []
-                    wind_result.append(hurricane_ai.plotting_utils._generate_sparse_feature_vector(11, 2, result[day]))
+                    wind_result.append(hurricane_ai.plotting_utils._generate_sparse_feature_vector(11, 2, result[day])) #
                     print(f'{day + 1} day: singular result wind test:{scaler.inverse_transform(wind_result)[0][2]}')
                 
                 elif featuresearch == 'sequential_1':
--- run.py
+++ run.py
@@ -97,7 +97,7 @@ def singular() :
     log(pprint.PrettyPrinter(indent=4).pprint(bidir_lstm_model_wind.model.get_config()))
     bidir_lstm_model_wind_hist = bidir_lstm_model_wind.train(X_train, y_train_wind, load_if_exists = args.load,
                                                            epochs = args.epochs)
-    
+"""
     log('Create and train bidirectional LSTM track model')
     bidir_lstm_model_lat = BidrectionalLstmHurricaneModel((X_train.shape[1], X_train.shape[2]), 'lat', feature_scaler,
                                                           dropout=args.dropout, loss=args.loss,
@@ -112,17 +112,18 @@ def singular() :
     log(pprint.PrettyPrinter(indent=4).pprint(bidir_lstm_model_lon.model.get_config()))
     bidir_lstm_model_lon_hist = bidir_lstm_model_lon.train(X_train, y_train_lon, load_if_exists = args.load,
                                                            epochs = args.epochs)
+"""
     
+
         return {
             'wind' : (bidir_lstm_model_wind, bidir_lstm_model_wind_hist),
             'lat' : (bidir_lstm_model_lat, bidir_lstm_model_lat_hist),
-            'lon' : (bidir_lstm_model_lon, bidir_lstm_model_lon_hist)
-        }
+            'long' : (bidir_lstm_model_lon, bidir_lstm_model_lon_hist)}
             
 
 def universal() :
     log('Create universal features')
-    log('Train for wind intensity (index 0), lat (index 1), lon (index 2).')
+    log('Train for wind intensity (index 0), lat (index 1), long (index 2).')
     global y_train, y_test, X_train, X_test, args, feature_scaler
     y_train = np.array([[[features[2], features[0], features[1]] for features in y] for y in y_train], dtype = np.float64)
     y_test = np.array([[[features[2], features[0], features[1]] for features in y] for y in y_test], dtype = np.float64)
